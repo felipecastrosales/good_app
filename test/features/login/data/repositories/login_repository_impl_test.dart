4aabc521b4c86a5f6422b6fdd2d28162c3a068f8
Felipe Sales <soufeliposales@gmail.com>
2022-11-14 22:00:24 -0300

    test: created test when has error on login

--- test/features/login/data/repositories/login_repository_impl_test.dart
+++ test/features/login/data/repositories/login_repository_impl_test.dart
@@ -1,7 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:good_app/features/login/data/repositories/repositories.dart';
 import 'package:mocktail/mocktail.dart';
+
+import 'package:good_app/app/app.dart';
+import 'package:good_app/features/login/data/repositories/repositories.dart';
 
 import '../../../../fixtures/fixtures.dart';
 
@@ -38,6 +40,32 @@ void main() {
 
     expect(result, isA<Right>());
     expect(result, Right(user.tUser));
+
+    verify(
+      () => loginDataSource.login(
+        username: any(named: 'username'),
+        password: any(named: 'password'),
+      ),
+    ).called(1);
+
+    verifyNoMoreInteractions(loginDataSource);
+  });
+
+  test('Should error when do login', () async {
+    when(
+      () => loginDataSource.login(
+        username: any(named: 'username'),
+        password: any(named: 'password'),
+      ),
+    ).thenThrow(PasswordWrongFailure());
+
+    var result = await loginRepositoryImpl.login(
+      username: user.username,
+      password: user.password,
+    );
+
+    expect(result, isA<Left>());
+    expect(result, Left(PasswordWrongFailure()));
 
     verify(
       () => loginDataSource.login(
