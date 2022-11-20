736d99d23ac54a1cd47cdc2c45e64fdc83abfac5
Felipe Sales <soufeliposales@gmail.com>
2022-11-19 22:33:39 -0300

    wip: remove to improve coverage

--- lib/app/core/ui/theme/app_theme.dart
+++ lib/app/core/ui/theme/app_theme.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:good_app/app/core/ui/ui.dart';
 
 class AppTheme {
-  AppTheme._();
   static ThemeData get theme => ThemeData(
         scaffoldBackgroundColor: AppColors.background,
         textSelectionTheme: const TextSelectionThemeData(
