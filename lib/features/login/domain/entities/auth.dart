a6957449401e7bcfb95dc508ad7f0289f99acd81
Felipe Sales <soufeliposales@gmail.com>
2022-12-14 21:40:40 -0300

    build: added freezed and related dependencies

--- lib/features/login/domain/entities/auth.dart
+++ lib/features/login/domain/entities/auth.dart
@@ -1,1 +1,13 @@
-class Auth {}
+class AuthEntity {
+  const AuthEntity({
+    required this.accessToken,
+    required this.refreshToken,
+    required this.expiresIn,
+  });
+
+  final String accessToken;
+  final String refreshToken;
+  final int expiresIn;
+
+  bool get isExpired => expiresIn < DateTime.now().millisecondsSinceEpoch;
+}
